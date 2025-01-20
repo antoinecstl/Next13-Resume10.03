3b2382de768efa43e0529a4dcd6f9a012863f105
Antoine CASTEL <antoine.castel@student-cs.fr>
2025-01-20 16:50:20 +0100

    FIX : remove layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,36 +3,8 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from "@vercel/analytics/react"
-import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
-
-function ScreenSizeIndicator() {
-  const [screenSize, setScreenSize] = useState('');
-
-  useEffect(() => {
-    const updateScreenSize = () => {
-      if (window.innerWidth < 640) {
-        setScreenSize('sm');
-      } else if (window.innerWidth < 768) {
-        setScreenSize('md');
-      } else if (window.innerWidth < 1024) {
-        setScreenSize('lg');
-      } else if (window.innerWidth < 1280) {
-        setScreenSize('xl');
-      } else if (window.innerWidth < 1536) {
-        setScreenSize('2xl');
-      }
-    };
-
-    updateScreenSize();
-    window.addEventListener('resize', updateScreenSize);
-
-    return () => window.removeEventListener('resize', updateScreenSize);
-  }, []);
-
-  return <div>Current screen size: {screenSize}</div>;
-}
 
 export default function RootLayout({
   children,
@@ -42,7 +14,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ScreenSizeIndicator />
         {children}
         <Analytics />
       </body>
